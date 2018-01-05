9af4f0961dbeaa8dc47bb0f804394f47af147169
codetesthenokh <Henokh.Santoso@mitrais.com>
2018-01-05 15:34:31 +0800

    Using AdminLTE

---
+++ database/migrations/2018_01_05_063210_create_todo_list_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTodoListTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('todo_list', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->string('title', 150);
+            $table->string('description', 300)->nullable();
+            $table->integer('priority');
+            $table->datetime('due_date')->nullable();
+            $table->boolean('is_completed')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('todo_list');
+    }
+}
